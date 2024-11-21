b930d3efbe0c1bd3f9b659e993bc6f7ea81b324b
Panos Hatzinikolaou <paahndev@gmail.com>
2024-11-20 16:10:34 -0800

    app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MoreTextComponent } from './more-text/more-text.component';
+import { LorumIpsumWithImgComponent } from './lorum-ipsum-with-img/lorum-ipsum-with-img.component';
+import { ScreenshotComponent } from "./screenshot/screenshot.component";
+import { NgxScreenshotComponent } from "./ngx-screenshot/ngx-screenshot.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    MoreTextComponent,
+    LorumIpsumWithImgComponent,
+    ScreenshotComponent,
+    NgxScreenshotComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
